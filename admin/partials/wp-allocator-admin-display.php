ec5bed7a5f2c50dbf0a1717909856f4b84c3d5a3
Pablo Marchena <pablo.marchena@orbitdevs.com>
2018-08-15 10:57:07 -0600

    Plugin boilerplate

---
+++ admin/partials/wp-allocator-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       http://orbitdevs.com/
+ * @since      1.0.0
+ *
+ * @package    Wp_Allocator
+ * @subpackage Wp_Allocator/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
---
+++ includes/class-wp-allocator-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://orbitdevs.com/
+ * @since      1.0.0
+ *
+ * @package    Wp_Allocator
+ * @subpackage Wp_Allocator/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Allocator
+ * @subpackage Wp_Allocator/includes
+ * @author     Pablo Marchena <clients@orbitdevs.com>
+ */
+class Wp_Allocator_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
